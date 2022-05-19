8fb0ec4ce66fa9e9b254b7417b955379b49a8a06
congly <daocongly0510@gmail.com>
2022-05-19 16:48:00 +0700

    cai dat modul

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem,Button, Form, FormGroup, Label, Input, Col,Row,FormFeedback } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem,Button,  Label, Col,Row, } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <=len);
+const minLength = (len) => (val) => !(val) && (val.length >=len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2.4}$/i.test(val);
+
 
 class Contact extends React.Component {
     constructor(props) {
@@ -64,7 +71,20 @@ class Contact extends React.Component {
                                     <Control.text model =".fristname" id="firstname" name="firstname"
                                     placeholder="First Name"
                                     className="form-control"
+                                    validator={{
+                                        required,minLength: minLength(3), maxLength: maxLength(15)
+                                    }}
                                     />
+                                    <Errors 
+                                    className="text-danger"
+                                    model=".firstname"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: "Must be greater than 2 characters",
+                                        maxLength: "Must be 15 characters or less"
+                                    }}
+                                    ></Errors>
                                 </Col>
                             </Row>
                             <Row className="form-group">
